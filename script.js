b01b3912b52db113a1a68d160e4be5affc23349d
joupan <zouvanlove20@gmail.com>
2024-11-27 06:23:34 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,229 @@
+const eskulData = [
+    {
+        id: 'basket',
+        name: 'Basket',
+        description: 'Bola basket (basketball) adalah olahraga bola berkelompok yang terdiri atas dua tim beranggotakan masing-masing lima orang yang saling bertanding mencetak poin dengan memasukkan bola ke dalam keranjang lawan. Bola basket dapat di lapangan terbuka, walaupun pertandingan profesional pada umumnya dilakukan di ruang tertutup. Lapangan pertandingan yang diperlukan juga relatif tidak besar, misal dibandingkan dengan sepak bola.',
+        details: [
+            'Ini adalah dokumentasi dari eskul Basket 103'
+        ],
+        images: [
+            "https://i.postimg.cc/yd5wGMKS/Whats-App-Image-2024-11-07-at-06-47-18.jpg",
+            "https://i.postimg.cc/4xLwwzsY/Whats-App-Image-2024-11-07-at-07-12-22.jpg", 
+            "https://i.postimg.cc/HWw2FGTD/Whats-App-Image-2024-11-07-at-07-13-30.jpg"
+        ]
+    },
+    {
+        id: 'volleyball',
+        name: 'Voli',
+        description: 'Bola voli merupakan permainan beregu yang bertujuan untuk memukul bola ke arah bidang lapangan lawan untuk mendapatkan poin. Menurut PBVSI bola voli merupakan permainan yang dimainkan oleh dua tim dalam setiap tim dan dipisahkan oleh sebuah net, permainan menggunakan tangan dengan cara dipantulkan atau dipukul.',
+        details: [
+            'ini adalah dokumentasi dari eskul Voli 103'
+        ],
+        images:[
+            "https://i.postimg.cc/vTZsRrgp/Whats-App-Image-2024-11-06-at-05-29-50-1.jpg", 
+            "https://i.postimg.cc/VNPPsQ09/Whats-App-Image-2024-11-07-at-05-27-52.jpg", 
+            "https://i.postimg.cc/gk5nDJ1Y/Whats-App-Image-2024-11-07-at-07-26-53.jpg"
+
+        ]
+    },
+    {
+        id: 'Futsal', 
+        name: 'Futsal', 
+        description:'Futsal adalah jenis sepak bola dalam ruangan yang dimainkan oleh dua tim, yang masing-masing beranggotakan lima orang. Tujuannya adalah memasukkan bola ke gawang lawan, dengan memanipulasi bola dengan kaki. Selain lima pemain utama, setiap regu juga diizinkan memiliki pemain cadangan. Tidak seperti permainan sepak bola dalam ruangan lainnya, lapangan futsal dibatasi garis, bukan net atau papan.', 
+        details: [
+            'Ini adalah dokumentasi dari eskul Futsal 103'
+        ], 
+        images: [
+            "https://i.postimg.cc/hvfsDYMS/Whats-App-Image-2024-11-06-at-05-29-50.jpg", 
+            "https://i.postimg.cc/NMGbVFH2/Whats-App-Image-2024-11-06-at-13-51-51.jpg", 
+            "https://i.postimg.cc/zv21qyT3/Whats-App-Image-2024-11-07-at-05-21-00.jpg"
+        ]
+    },
+    {
+        id: 'KIR',
+        name: 'KIR',
+        description: 'Singkatan dari Kelompok Ilmiah Remaja, adalah kegiatan ekstrakurikuler di sekolah yang berfokus pada pengembangan minat dan kemampuan siswa dalam bidang penelitian ilmiah. Dalam eskul ini, siswa dilatih untuk memahami metode ilmiah, melakukan eksperimen, dan menulis laporan penelitian. Eskul KIR biasanya mencakup berbagai bidang ilmu, seperti sains, teknologi, lingkungan, sosial, dan lainnya, sehingga memungkinkan siswa untuk mengasah keterampilan berpikir kritis, analitis, dan kreatif. Melalui KIR, siswa tidak hanya belajar teori, tetapi juga langsung menerapkannya melalui proyek-proyek penelitian yang melibatkan pengamatan, eksperimen, dan analisis data.',
+        details: [
+            'Ini adalah dokumentasi dari eskul KIR 103'
+        ],
+        images:[
+            "https://i.postimg.cc/x805wCNb/Whats-App-Image-2024-11-05-at-06-39-11.jpg",
+            "https://i.postimg.cc/DfgBqqRQ/Whats-App-Image-2024-11-05-at-06-39-12.jpg",
+            "https://i.postimg.cc/jdSvvDrp/Whats-App-Image-2024-11-05-at-06-39-12-1.jpg"
+        ]
+    },
+    {
+        id: 'rohis',
+        name: 'Rohis',
+        description: 'Eskul Rohis, singkatan dari "Rohani Islam," adalah kegiatan ekstrakurikuler di sekolah yang bertujuan untuk memperdalam pemahaman siswa mengenai ajaran Islam. Dalam eskul Rohis, siswa mempelajari berbagai aspek keislaman, seperti membaca dan menghafal Al-Qur’an, memahami makna hadits, mengkaji fiqh (hukum Islam), dan mempelajari sejarah Nabi Muhammad SAW dan para sahabatnya. Eskul ini tidak hanya berfokus pada pengajaran teori agama, tetapi juga menekankan penerapan nilai-nilai Islam dalam kehidupan sehari-hari. Kegiatan Rohis sering kali mencakup pengajian, ceramah, diskusi, mentoring, serta kegiatan sosial yang menanamkan kepedulian dan akhlak mulia.',
+        details: [
+            'Ini adalah dokumentasi dari eskul Rohis 103'
+        ],
+        images: [
+            "https://i.postimg.cc/ydSW7X9M/IMG-6753.jpg", 
+            "https://i.postimg.cc/L4Yjsj26/20241004-070707.jpg", 
+            "https://i.postimg.cc/6pY8cQtm/20241004-080701.jpg"
+        ]
+    },
+    {
+        id: 'fragma',
+        name: 'Fragma',
+        description: 'Singkatan dari Fotografi dan Sinematografi, sebuah kegiatan ekstrakurikuler di sekolah yang berfokus pada pengembangan keterampilan siswa dalam bidang fotografi dan sinematografi. Dalam eskul ini, siswa diajarkan teknik-teknik dasar fotografi, seperti komposisi, pencahayaan, dan penggunaan kamera, serta teknik sinematografi, termasuk pembuatan skenario, pengambilan gambar, dan penyuntingan video. Eskul Fragma bertujuan untuk mengembangkan kreativitas dan kemampuan siswa dalam menghasilkan karya visual yang memiliki nilai estetika serta pesan yang kuat. Selain itu, eskul ini juga mendorong siswa untuk lebih peka terhadap lingkungan sekitar dan mampu mengabadikan momen-momen berharga dalam bentuk foto atau video.',
+        details: [
+            'Ini adalah dokumentasi eskul fragma 103'
+        ],
+        images: [
+            "https://i.postimg.cc/zf03BhZD/Whats-App-Image-2024-11-04-at-19-55-39.jpg",
+            "https://i.postimg.cc/MK61xNPV/Whats-App-Image-2024-11-04-at-19-55-39-1.jpg",
+            "https://i.postimg.cc/Y2fQ5M3t/Whats-App-Image-2024-11-04-at-19-55-40.jpg"
+        ]
+    },
+    { 
+        id: 'Bulu Tangkis',
+        name: 'Bulu Tangkis',
+        description: 'Bulu tangkis atau tepuk bulu (badminton) adalah suatu olahraga raket yang menggunakan raket untuk memukul kok melewati jaring. Meskipun dapat dimainkan dengan tim yang lebih besar, bentuk permainan yang paling umum adalah "tunggal" (dengan satu pemain di setiap sisi) dan "ganda" (dengan dua pemain di setiap sisi) yang saling berlawanan.',
+        details: [
+            "Ini adalah dokumentasi eskul Bulu Tangkis 103"
+        ], 
+        images: [
+            "https://i.postimg.cc/05XgMJJy/Whats-App-Image-2024-11-06-at-05-29-50-3.jpg", 
+            "https://i.postimg.cc/281s484F/Whats-App-Image-2024-11-06-at-13-54-34-1.jpg", 
+            "https://i.postimg.cc/4NjrwkPC/Whats-App-Image-2024-11-07-at-06-43-00-1.jpg" 
+        ]
+    },
+    {
+        id: 'Band',
+        name:'Band', 
+        description: 'Band merupakan salah satu kegiatan yang diselenggarakan diluar jam pelajaran sekolah. Kegiatan ekstrakurikuler Band ini dimaksudkan untuk menambah kemampuan perseptual yang meliputi kepekaan indrawi terhadap bunyi dan kreatifitas dalam berkarya dan berimajinasi. Ekstrakurikuler Band sebagai salah satu kegiatan penyaluran dan pengembangan bakat minat yang dimiliki oleh peserta didik di SMAN 103 Jakarta',
+        details: [
+            'Ini adalah dokumentasi eskul Band 103' 
+        ], 
+        images: [
+            "https://i.postimg.cc/B6ZytqSF/Whats-App-Image-2024-11-07-at-06-18-29.jpg", 
+            "https://i.postimg.cc/TwksDZ14/Whats-App-Image-2024-11-07-at-06-13-37.jpg", 
+            "https://i.postimg.cc/4ymSbBhN/Whats-App-Image-2024-11-07-at-06-13-36-2.jpg"
+        ]
+    },
+    {
+        id: 'PMR', 
+        name:'PMR', 
+        description: 'Palang Merah Remaja atau PMR adalah suatu organisasi binaan dari Palang Merah Indonesia yang berpusat di sekolah-sekolah ataupun kelompok-kelompok masyarakat (sanggar, kelompok belajar, dll.) yang bertujuan membangun dan mengembangkan karakter Kepalangmerahan agar siap menjadi Relawan PMI pada masa depan..',
+        details:[
+            'Ini adalah dokumentasi eskul PMR 103'
+        ],
+        images:[
+            "https://i.postimg.cc/N0BCPgrn/Whats-App-Image-2024-11-07-at-06-24-08.jpg",
+            "https://i.postimg.cc/k451qfsF/Whats-App-Image-2024-11-07-at-06-24-17.jpg",
+            "https://i.postimg.cc/QMm42RQd/Whats-App-Image-2024-11-07-at-06-24-26.jpg"
+        ]
+    }, 
+    {
+        id: 'Tari Ratoh Jaroe',
+        name: 'Tari Ratoh Jaroe', 
+        description:'Tari Ratoh Jaroe berasal dari kata “ratoh” yang berarti “berkata” atau “berbincang”; dan “jaroe” yang berarti “jari tangan”. Jadi, dari asal-usul katanya, tari Ratoh Jaroe berarti sebuah tarian dengan melantunkan syair atau menuturkan kisah sambil diiringi petikan-petikan jari tangan.Ciri khas Tari Ratoh Jaroe adalah koreografi gerakannya yang sporadis, ritmis, namun tetap mempertahankan estetika komposisi dan keluwesan gerakan para penarinya. Tari Ratoh Jaroe memiliki 33 gerakan dan lima kali pengulangan gerak. Penari secara khusus menggunakan tubuh bagian atas terutama tangan, lengan, dan kepala untuk melakukan koreografi dalam posisi duduk. Mereka menyanyikan lagu-lagu dalam bahasa Aceh mengikuti tempo yang dimainkan dua penabuh rapai',
+        details:[
+            'Ini adalah dokumentasi Tari Ratoh Jaroe'
+        ],
+        images:[
+            "https://i.postimg.cc/CLvKK1wG/Whats-App-Image-2024-11-07-at-06-13-35.jpg",
+            "https://i.postimg.cc/FH7Hk4K2/Whats-App-Image-2024-11-07-at-06-13-36.jpg",
+            "https://i.postimg.cc/ryNmvjbY/Whats-App-Image-2024-11-07-at-06-18-28.jpg"
+
+        ]
+    },
+    {
+        id: 'Modern Dance', 
+        name: 'Modern Dance', 
+        description:' Ekstrakurikuler (ekskul) modern dance adalah wadah untuk menyalurkan minat, bakat, dan kreativitas siswa dalam bidang tarian modern. Beberapa hal yang dapat dipelajari dalam ekskul modern dance, yaitu Mengembangkan keterampilan tari. Memahami musik, ritme, dan ekspresi emosional.',
+        details:[
+            'Ini adalah dokumentasi Modern Dance'
+        ],
+        images:[
+            "https://i.postimg.cc/7Ykb7T20/Whats-App-Image-2024-11-08-at-07-49-38-1.jpg",
+            "https://i.postimg.cc/65fqh5rS/Whats-App-Image-2024-11-08-at-07-49-39-1.jpg",
+            "https://i.postimg.cc/7Ykb7T20/Whats-App-Image-2024-11-08-at-07-49-38-1.jpg"
+        ]
+    },
+    {
+        id:'Hadroh', 
+        name: 'Hadroh', 
+        description:'Eskul Hadroh adalah kegiatan ekstrakurikuler di sekolah yang berfokus pada seni musik Islami, khususnya memainkan alat musik hadroh. Hadroh merupakan alat musik tradisional Islami yang biasanya terdiri dari beberapa jenis rebana atau gendang kecil dan alat musik pendukung lainnya. Musik hadroh biasanya dimainkan untuk mengiringi syair-syair pujian dan shalawat kepada Nabi Muhammad SAW, sehingga memiliki nuansa religius yang mendalam. Selain sebagai bentuk ibadah, eskul hadroh juga bertujuan melestarikan seni budaya Islami serta menanamkan kecintaan kepada Nabi Muhammad SAW melalui seni musik.', 
+        details: [
+            'Ini adalah dokumentasi Hadroh 103' 
+        ],
+        images: [
+            "https://i.postimg.cc/NML1Qy73/Whats-App-Image-2024-11-05-at-06-37-04-1.jpg", 
+            "https://i.postimg.cc/FFdjqLKc/Whats-App-Image-2024-11-05-at-06-37-05-1.jpg",
+            "https://i.postimg.cc/NjYm5Ck7/Whats-App-Image-2024-11-05-at-06-37-05.jpg" 
+        ]
+    }
+];
+
+function displayResults(results) {
+    const resultContainer = document.getElementById('result');
+    resultContainer.innerHTML = '';
+
+    if (results.length === 0) {
+        resultContainer.innerHTML = '<p>Tidak ada hasil yang ditemukan.</p>';
+    } else {
+        results.forEach(eskul => {
+            const eskulElement = document.createElement('div');
+            eskulElement.classList.add('eskul-item');
+            eskulElement.innerHTML = `
+                <h2>${eskul.name}</h2>
+                <p>${eskul.description}</p>
+                <ul>
+                    ${eskul.details.map(detail => `<li>${detail}</li>`).join('')}
+                </ul>
+                 <div class="image-gallery">
+                    ${eskul.images.map(image => `<img src="${image}" alt="${eskul.name}" class="gallery-image">`).join('')}
+                </div>
+            `;
+            resultContainer.appendChild(eskulElement);
+        });
+    }
+
+    resultContainer.classList.add('show');
+}
+
+function searchEskul(query) {
+    return eskulData.filter(eskul => 
+        eskul.name.toLowerCase().includes(query.toLowerCase())
+    );
+}
+
+
+function displayAllEskul() {
+    const resultContainer = document.getElementById('result');
+    resultContainer.innerHTML = '<ul class="eskul-list">';
+
+    eskulData.forEach(eskul => {
+        resultContainer.innerHTML += `
+            <li>${eskul.name}</li>
+        `;
+    });
+
+    resultContainer.innerHTML += '</ul>';
+    resultContainer.classList.add('show');
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    displayAllEskul(); 
+
+    document.getElementById('search-input').addEventListener('input', function(e) {
+        const query = e.target.value.trim(); 
+        const resultContainer = document.getElementById('result');
+
+        const minLength = 3;
+
+        if (query.length >= minLength) { 
+            const results = searchEskul(query); 
+            displayResults(results); 
+        } else if (query.length === 0) {
+            displayAllEskul(); 
+        } else {
+            resultContainer.innerHTML = ''; 
+            resultContainer.classList.remove('show'); 
+        }
+    });
+});
